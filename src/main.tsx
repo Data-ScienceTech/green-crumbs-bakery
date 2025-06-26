3afb194c68d54bef8e87fdec5a8dd2d31911c71b
Carlos Denner <carlosdenner@gmail.com>
2025-06-26 19:13:37 -0400

    debug: add BASE_URL logging to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+console.log('BASE_URL:', import.meta.env.BASE_URL);
+
 
 createRoot(document.getElementById("root")!).render(<App />);
